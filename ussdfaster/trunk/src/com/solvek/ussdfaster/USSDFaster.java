1d1bf7dc1a9b4f8c06ebdab08347748faae72d43
adamchuk <adamchuk@813c62f2-f032-0410-b7bf-bdd43f9d1d0a>
2010-05-01 22:20:06 +0000

    Created activity for Group

--- ussdfaster/trunk/src/com/solvek/ussdfaster/USSDFaster.java
+++ ussdfaster/trunk/src/com/solvek/ussdfaster/USSDFaster.java
@@ -1,20 +1,19 @@
 package com.solvek.ussdfaster;
 
-import java.io.IOException;
-
-import org.xmlpull.v1.XmlPullParserException;
-
 import com.solvek.ussdfaster.entities.Carrier;
 import com.solvek.ussdfaster.parsers.CarrierParser;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
-public class USSDFaster extends Activity {
+public class USSDFaster extends TabActivity {
 	static final private int MENU_QUIT = Menu.FIRST;
 	
     @Override
@@ -24,17 +23,33 @@ public class USSDFaster extends Activity {
         
         XmlResourceParser xml = getResources().getXml(R.xml.mts);
         CarrierParser parser = new CarrierParser(xml);
+        Carrier c;
         try {
-			Carrier c = parser.parse();
+			c = parser.parse();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			finish();
 		}
+		
+		Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    Intent intent = new Intent().setClass(this, GroupActivity.class);
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    TabHost.TabSpec spec = tabHost
+	    	.newTabSpec("artists")
+	    	.setIndicator("Finances", res.getDrawable(R.drawable.ic_tab_artists))
+	        .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    tabHost.setCurrentTab(0);
     }
     
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, MENU_QUIT, 0, "Quit");
+        menu.add(0, MENU_QUIT, 0, R.string.quit);
         return true;
     }
     
