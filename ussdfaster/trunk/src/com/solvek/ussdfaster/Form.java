c4037eccb5b41acfd058df3c51a4c3a5b7de9474
adamchuk <adamchuk@813c62f2-f032-0410-b7bf-bdd43f9d1d0a>
2010-05-05 05:22:07 +0000

    Form dev continued

--- ussdfaster/trunk/src/com/solvek/ussdfaster/Form.java
+++ ussdfaster/trunk/src/com/solvek/ussdfaster/Form.java
@@ -1,13 +1,24 @@
 package com.solvek.ussdfaster;
 
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.solvek.ussdfaster.entities.Command;
+import com.solvek.ussdfaster.fields.BaseField;
 
 public final class Form extends AlertDialog {
 	private Command command;
+	private List<BaseField> fields;
+	private HashMap<String, View> views;
+	private HashMap<String, BaseField> fset;
 
 	public Form(Context context, DialogInterface.OnClickListener listener, Command command) {
 		super(context);
@@ -28,15 +39,39 @@ public final class Form extends AlertDialog {
 		                dialog.cancel();
 		           }});
 		
-    	/*List<BaseField> fields = command.getFields();
+    	fields = command.getFields();
     	
-    	HashMap<String, View> views = new HashMap<String, View>(fields.size());
-    	HashMap<String, BaseField> fset = new HashMap<String, BaseField>(fields.size());*/		
+    	views = new HashMap<String, View>(fields.size());
+    	fset = new HashMap<String, BaseField>(fields.size());
+    	
+    	LinearLayout content = new LinearLayout(context);
+    	content.setOrientation(LinearLayout.VERTICAL);
+    	this.setView(content);
+    	
+    	//LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	//vi.inflate(R.layout.mycustomlayout,this,true, null); 
+    	for(BaseField field : fields){
+    		fset.put(field.getName(), field);
+    		
+    		TextView label = new TextView(context);
+    		label.setText(field.getName());
+    		
+    		content.addView(label);
+    		
+    		View editor = field.createView();
+    		//content.addView(editor);
+    		
+    		views.put(field.getName(), editor);
+    	}
 	}
 	
 	public String getFormReplaces(){
     	/*HashMap<String, String> res = new HashMap<String, String>(command.getFields().size());		
 		return res;*/
 		return this.command.getTemplate();
+	}
+	
+	private void createForm(){
+		
 	}
 }
